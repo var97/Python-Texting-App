bfcfd340f88cffb9d6545b93b09d745493d3a97c
Varun Agarwal <varun1997agarwal@gmail.com>
2018-06-23 13:24:02 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = 'ACbc0a089e44586481d749d8bc64a5d3e2'
-auth_token = 'cffee92b7e7eb5f1dbcc0aab1a4329f4'
-my_cell = '+919694207461'
-my_twilio = '+17402245835'
+account_sid = 'Your account_sid goes here'
+auth_token = 'Your auth_token goes here'
+my_cell = 'Your verified Cell No goes here'
+my_twilio = 'Your twilio No goes here'
